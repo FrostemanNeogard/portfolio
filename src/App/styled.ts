72f0e7ab9572b7e56acc87454dad8677e88b53d1
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2024-03-14 15:43:07 +0100

    Add basic styling

--- src/App/styled.ts
+++ src/App/styled.ts
@@ -2,4 +2,9 @@ import { styled } from "styled-components";
 
 export const AppContainer = styled.div`
   font-family: ${({ theme }) => theme.primaryFontFamily};
+  min-height: 100vh;
+  height: fit-content;
+  padding: 0 20vmax;
+  background-color: ${({ theme }) => theme.primary};
+  color: ${({ theme }) => theme.primaryText};
 `;
