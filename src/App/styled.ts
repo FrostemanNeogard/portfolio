ccda13b674cc70eafc22fbeece646149cad1cbd2
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2024-03-15 23:51:54 +0100

    Reduce padding

--- src/App/styled.ts
+++ src/App/styled.ts
@@ -4,7 +4,7 @@ export const AppContainer = styled.div`
   font-family: ${({ theme }) => theme.primaryFontFamily};
   min-height: 100vh;
   height: fit-content;
-  padding: 0 20vw;
+  padding: 0 11vw;
   @media ${(props) => props.theme.breakpoints.mobile} {
     padding: 0 3rem;
   }
