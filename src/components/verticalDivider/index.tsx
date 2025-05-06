a9000ea9d9fbad7b77f9bd270d3ae929304a5da6
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2025-05-06 15:59:38 +0200

    Remove unused components

--- src/components/verticalDivider/index.tsx
+++
@@ -1,7 +0,0 @@
-import * as Styled from "./styled";
-
-const VerticalDivider = () => {
-  return <Styled.VerticalDivider />;
-};
-
-export default VerticalDivider;
--- src/components/verticalDivider/styled.ts
+++
@@ -1,7 +0,0 @@
-import { styled } from "styled-components";
-
-export const VerticalDivider = styled.div`
-  border-left: 1px solid ${(props) => props.theme.dividerColor};
-  height: 100%;
-  margin: 0 1em;
-`;
